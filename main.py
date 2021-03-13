293a56b30fd7284dad0470d7318a0f1f003245ed
JesperKYH <jesper.pettersson@student.kyh.se>
2021-03-13 13:24:41 +0100

    Done with window

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import pygame
 pygame.init()
 dis = pygame.display.set_mode((400, 300))
 pygame.display.update()
+# This is what you see at the top of the window
 pygame.display.set_caption('Snake Game')
 game_over = False
 while not game_over:
     for event in pygame.event.get():
-        print(event)    # prints out all the actions that take place on the screen
+        if event.type == pygame.QUIT:
+            game_over = True
+
 pygame.quit()
 quit()
